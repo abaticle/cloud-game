9e4624963c02b594a37b4f79229654080c129a9f
Sergey Stepanov <sergystepanov@gmail.com>
2020-04-27 23:37:08 +0300

    Update settings

--- web/js/settings/settings.js
+++ web/js/settings/settings.js
@@ -1,25 +1,64 @@
+/**
+ * App settings module.
+ *
+ * @version 1
+ */
 const settings = (() => {
-    const settingsKey = 'settings';
     const revision = 1;
-    let isProviderSupported = _isSupported();
-
-    // !to do something if localStorage is unavailable
 
     let settings = {
         _version: revision
     };
+
+    const localStorageProvider = (() => {
+        const settingsKey = 'settings';
+        const isSupported = _isSupported();
+
+        const _save = () => localStorage.setItem(settingsKey, _export(settings));
+
+        function _isSupported() {
+            const testKey = 'test_' + Math.random().toString(36).substring(5);
+            try {
+                localStorage.setItem(testKey, testKey);
+                localStorage.removeItem(testKey);
+                return true;
+            } catch (e) {
+                return false;
+            }
+        }
+
+        const get = (key) => JSON.parse(localStorage.getItem(key));
+
+        const set = (key, value) => {
+            settings[key] = value;
+            _save();
+        }
+
+        const loadSettings = () => {
+            if (!localStorage.getItem(settingsKey)) _save();
+            settings = JSON.parse(localStorage.getItem(settingsKey));
+        }
+
+        return {
+            get,
+            set,
+            loadSettings,
+            isSupported,
+        }
+    });
+
+    // setting a data provider
+    let provider = localStorageProvider();
 
     const _import = () => {
     }
 
     const _export = (data) => JSON.stringify(data, null, 2);
 
-    const load = () => {
-        if (!isProviderSupported) return;
+    const init = () => {
+        if (!provider.isSupported) return;
 
-        if (!_hasSettings()) _save();
-
-        settings = _load();
+        provider.loadSettings();
 
         if (revision !== settings._version) {
             // !to handle this as migrations
@@ -29,39 +68,15 @@ const settings = (() => {
     const get = () => settings
 
     const loadOr = (key, default_) => {
-        if (!isProviderSupported) return default_;
-        if (settings.hasOwnProperty(key)) return settings[key];
-
-        settings[key] = default_;
-        _save();
+        if (provider.isSupported) {
+            if (settings.hasOwnProperty(key)) return settings[key];
+            provider.set(key, default_);
+        }
 
         return default_;
     }
 
-    function _hasSettings() {
-        return localStorage.getItem(settingsKey);
-    }
-
-    function _save() {
-        localStorage.setItem(settingsKey, _export(settings));
-    }
-
-    function _load() {
-        return JSON.parse(localStorage.getItem(settingsKey));
-    }
-
-    function _isSupported() {
-        const testKey = 'test_' + Math.random().toString(36).substring(5);
-        try {
-            localStorage.setItem(testKey, testKey);
-            localStorage.removeItem(testKey);
-            return true;
-        } catch (e) {
-            return false;
-        }
-    }
-
-    load();
+    init();
 
     return {
         get,
